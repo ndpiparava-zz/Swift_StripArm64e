46f3e9529c6ae1e62eff281996a5e125a53913df
Nitin <ndpiparava@gmail.com>
2020-01-29 15:16:52 +0530

    Script to remove arm64e from swift lib files
    This script to fix  IOS app Testflight upload issue.
    
    ITMS-90428: Invalid Swift Support - The files libswiftDarwin.dylib, libswiftObjectiveC.dylib, libswiftMetal.dylib, libswiftDispatch.dylib, libswiftCoreFoundation.dylib, libswiftCore.dylib, libswiftQuartzCore.dylib, libswiftUIKit.dylib, libswiftCoreImage.dylib, libswiftCoreLocation.dylib, libswiftos.dylib, libswiftCoreGraphics.dylib, libswiftFoundation.dylib don’t match /Payload/Vodafone.app/Frameworks/libswiftDarwin.dylib, /Payload/Vodafone.app/Frameworks/libswiftObjectiveC.dylib, /Payload/Vodafone.app/Frameworks/libswiftMetal.dylib, /Payload/Vodafone.app/Frameworks/libswiftDispatch.dylib,

---
+++ stripArm64e.sh
@@ -0,0 +1,21 @@
+
+
+function stripArm64e()
+{
+#Path to /usr/bin/lip , can find from xcode showcontent
+LIPO="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/lipo"
+
+if [[ '10.2.1' == "10."* ]]
+    then
+
+    #path to libswift inside .app file
+    echo -e "/Users/npiparav/SourceCodes/Vodafone/StripArm64e/Iris.app/Frameworks/libswift*"
+    for filename in /Users/npiparav/SourceCodes/Vodafone/StripArm64e/Iris.app/Frameworks/libswift*; do
+    $LIPO "$filename" -remove arm64e -output $filename
+    done
+fi
+}
+
+
+stripArm64e
+
